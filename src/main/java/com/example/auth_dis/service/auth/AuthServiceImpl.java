68c36b118aea5af7bd6a30b301347ab4b26818b1
jun <toinin@naver.com>
2020-12-15 19:27:54 +0900

    .

--- src/main/java/com/example/auth_dis/service/auth/AuthServiceImpl.java
+++ src/main/java/com/example/auth_dis/service/auth/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.auth_dis.Domain.Token;
 import com.example.auth_dis.Domain.user.User;
 import com.example.auth_dis.Domain.user.UserRepository;
 import com.example.auth_dis.Exception.PasswordNotFoundException;
+import com.example.auth_dis.Exception.TokenInvalidException;
 import com.example.auth_dis.Exception.UserNotFoundException;
 import com.example.auth_dis.jwt.JwtTokenUtil;
 
@@ -22,6 +23,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Ref;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -50,8 +52,8 @@ public class AuthServiceImpl implements AuthService {
         retok.setUsername(request.getId());
         retok.setRefreshToken(refreshToken);
         try {
-            ValueOperations<String, Object> vop = redisTemplate.opsForValue();
-            vop.set(request.getId(), retok);
+//            ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+//            vop.set(request.getId(), retok);
         }catch (Exception e){
             System.out.println("이거 왜안되냐고");
         }
@@ -73,17 +75,17 @@ public class AuthServiceImpl implements AuthService {
         User user = userRepository.findById(email)
                 .orElseThrow();
 
-        try {
-            if (redisTemplate.opsForValue().get(user.getEmail()) != null) {
-                //delete refresh token
-                redisTemplate.delete(user.getEmail());
-            }
-        } catch (IllegalArgumentException e) {
-            logger.warn("user does not exist");
-        }
+//        try {
+//            if (redisTemplate.opsForValue().get(user.getEmail()) != null) {
+//                //delete refresh token
+//                redisTemplate.delete(user.getEmail());
+//            }
+//        } catch (IllegalArgumentException e) {
+//            logger.warn("user does not exist");
+//        }
 
         logger.info(" logout ing : " + refreshToken);
-        redisTemplate.expire(refreshToken, 10 * 6 * 1000, TimeUnit.MILLISECONDS);
+        redisTemplate.expire(refreshToken,  24 * 60 * 60 * 7 * 1000, TimeUnit.MILLISECONDS);
 
         return new ResponseEntity(HttpStatus.OK);
     }
@@ -91,37 +93,8 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public TokenResponse GET_ACCESS_BY_REFRESH(String RefreshToken) {
-        String refreshToken;
-        String refreshTokenFromDb;
-        String username;
-        String newtok = null;
-        try {
-            refreshToken = RefreshToken;
-            logger.info("refresh token in db: " + refreshToken);
-
-            username = jwtTokenUtil.getId(refreshToken);
-
-            User user = userRepository.findById(username).orElseThrow();
-            String email = user.getEmail();
-
-            try {
-                ValueOperations<String, Object> vop = redisTemplate.opsForValue();
-                Token result;
-                result = (Token) vop.get(email);
-                refreshTokenFromDb = result.getRefreshToken();
-                logger.info("refresh token in db: " + refreshTokenFromDb);
-                if (refreshToken.equals(refreshTokenFromDb)) {
-                    logger.info("a");
-
-                    newtok = jwtTokenUtil.generateAccessToken(email);
-                }
-            } catch (IllegalArgumentException e) {
-                logger.warn("illegal argument!!");
-            }
-        } catch (Exception e) {
-            throw e;
-        }
-
+        if (!jwtTokenUtil.isRefreshToken(RefreshToken)) throw new TokenInvalidException("토큰 에러뿅");
+        String newtok = jwtTokenUtil.generateAccessToken(jwtTokenUtil.getId(RefreshToken));
         return new TokenResponse(newtok, RefreshToken);
     }
 }
