5c238f81959abc0e9cf4c9aa83925181de8358b0
jun <toinin@naver.com>
2020-12-15 14:46:22 +0900

    .

---
+++ src/main/java/com/example/auth_dis/config/EmbeddedConfig.java
@@ -0,0 +1,35 @@
+package com.example.auth_dis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class EmbeddedConfig {
+    @Profile("local")
+    @Configuration
+    public class RedisConfig {
+
+        @Value("${spring.redis.port}")
+        private int redisPort;
+
+        private RedisServer redisServer;
+
+        @PostConstruct
+        public void runRedis() {
+            redisServer = new RedisServer(redisPort);
+            redisServer.start();
+        }
+
+        @PreDestroy
+        public void stopRedis() {
+            if (redisServer != null) {
+                redisServer.stop();
+            }
+        }
+
+    }
+}
